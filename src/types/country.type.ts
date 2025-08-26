3a0bda50cfc97fcbc71d27d1380e279443670ea6
Marina Kholodova <marinaholodova@gmail.com>
2025-08-26 21:12:28 +0300

    feat: add service for get Co2 from owid-co2-data.json

---
+++ src/types/country.type.ts
@@ -0,0 +1,16 @@
+﻿export type CountryData = {
+  name: string;
+  series: CountrySeries;
+};
+
+export type CountrySeries = {
+  data: YearData[];
+  iso_code?: string;
+};
+
+export type YearData = {
+  year: number;
+  population?: number;
+  co2?: number;
+  co2_per_capita?: number;
+};
---
+++ src/workers/jsonWorker.ts
@@ -0,0 +1,12 @@
+﻿self.onmessage = async (e: MessageEvent) => {
+  const url = String(e.data);
+  try {
+    const resp = await fetch(url);
+    if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
+
+    const data = await resp.json();
+    postMessage({ ok: true, data });
+  } catch (error) {
+    postMessage({ ok: false, error: String(error) });
+  }
+};

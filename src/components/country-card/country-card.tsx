46b5fbdb3bd39c47dda73cc406266aa13fb4c664
Marina Kholodova <marinaholodova@gmail.com>
2025-08-26 21:25:13 +0300

    feat: add country card and country list

---
+++ src/components/country-card/country-card.tsx
@@ -0,0 +1,43 @@
+﻿import styles from './country-card.module.css';
+import type { CountryData } from '../../types/country.type.ts';
+import { useCallback, useState } from 'react';
+import DataTable from '../data-table/data-table.tsx';
+
+export type CountryCardProps = {
+  country: CountryData;
+  selectedColumns: string[];
+};
+
+export function CountryCard({ country, selectedColumns }: CountryCardProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  const data = country.series.data ?? [];
+  const latestYear = Math.max(
+    ...data.map((r) => Number(r.year)).filter(Number.isFinite)
+  );
+  const row = data.find((r) => Number(r.year) === latestYear);
+  const population = row?.population?.toString() ?? 'N/A';
+
+  const columns = ['year', ...selectedColumns];
+
+  const handleClick = useCallback(() => setExpanded((p) => !p), []);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.info}>
+        <h2 className={styles.title}>{country.name}</h2>
+        <p className={styles.text}>
+          Population : <span>{population}</span>
+        </p>
+        <p className={styles.text}>
+          ISO code: <span>{country.series.iso_code ?? 'N/A'}</span>
+        </p>
+      </div>
+
+      <button className={styles.button} onClick={handleClick}>
+        {expanded ? 'Skip table' : 'Show table'}
+      </button>
+      {expanded && <DataTable columns={columns} row={row ?? {}} />}
+    </div>
+  );
+}
---
+++ src/components/country-list/country-list.tsx
@@ -0,0 +1,25 @@
+﻿import styles from './country-list.module.css';
+import { CountryCard } from '../country-card/country-card.tsx';
+import type { CountryData } from '../../types/country.type.ts';
+
+export type CountryListProps = {
+  countries: CountryData[];
+  selectedColumns: string[];
+};
+
+export function CountryList({ countries, selectedColumns }: CountryListProps) {
+  if (!countries.length)
+    return <div className={styles.empty}>No countries to display</div>;
+
+  return (
+    <div className={styles.list}>
+      {countries.map((country) => (
+        <CountryCard
+          selectedColumns={selectedColumns}
+          key={country.name}
+          country={country}
+        />
+      ))}
+    </div>
+  );
+}

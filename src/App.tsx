c0540cb98f41b1b9d7db4b5748a165e4aa60c6a3
Marina Kholodova <marinaholodova@gmail.com>
2025-08-28 11:00:03 +0300

    fix: fix style for loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Suspense } from 'react';
-import './App.css';
+import styles from './app.module.css';
 import AppCore from './components/app-core/app-core.tsx';
 import CircleLoader from './components/circle-loader/circle-loader.tsx';
 
 function App() {
   return (
-    <Suspense fallback={<CircleLoader />}>
+    <Suspense
+      fallback={
+        <div className={styles.fallback}>
+          <CircleLoader />
+        </div>
+      }
+    >
       <AppCore />
     </Suspense>
   );
